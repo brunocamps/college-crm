10f0b38e17a9ca63c285f4621b4f159b1b957ea3
brunocamps <bruno.mcamps@outlook.com>
2022-08-24 20:52:09 -0300

    created services table (61)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_130632) do
+ActiveRecord::Schema.define(version: 2022_08_24_234311) do
 
   create_table "classrooms", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.integer "duration"
+    t.integer "client_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
