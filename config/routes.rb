a9eb166ca7d48323fe5799083d5101c3a7023813
brunocamps <bruno.mcamps@outlook.com>
2022-08-24 11:03:43 -0300

    Added Classroom scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :classrooms
   devise_for :users
   resources :users, only: [:index, :show, :destroy, :edit, :update]
   root "static_pages#landing_page"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_29_230108) do
+ActiveRecord::Schema.define(version: 2022_08_24_130632) do
+
+  create_table "classrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'inet' for column 'current_sign_in_ip'
