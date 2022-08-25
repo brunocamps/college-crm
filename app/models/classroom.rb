ae2a92eea69159a930b879d6686eb43ff9e6f164
brunocamps <bruno.mcamps@outlook.com>
2022-08-24 21:43:13 -0300

    61 - services validations

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,5 +1,6 @@
 class Classroom < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    validates_uniqueness_of :name, case_sentitive: false
 
     def to_s
         name
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,10 @@
 class Service < ApplicationRecord
+    validates :name, presence: true
+    validates_uniqueness_of :name, case_sentitive: false
+    validates :duration, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 180}
+    validates :client_price, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 10000}
+
+    def to_s
+        name
+    end
 end
